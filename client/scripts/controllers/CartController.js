549270038a22a26e78532a88ac620f0c27a9aac2
Anna Springfield <anna.e.springfield@gmail.com>
2017-04-12 17:27:57 -0500

    add cart logic

--- client/scripts/controllers/CartController.js
+++ client/scripts/controllers/CartController.js
@@ -1,6 +1,26 @@
 myApp.controller('CartController', ['PizzaOvenService', function(PizzaOvenService) {
 
   var cart = this;
-  
+
   cart.pizzaObject = PizzaOvenService.pizzaObject;
+
+  cart.createToppingsArray = function(pizza) {
+    console.log(pizza);
+    var toppingsArray = [];
+
+    for (var topping in pizza.toppings) {
+      console.log("pizza.toppings", pizza.toppings);
+      console.log("topping", topping);
+      if (pizza.toppings[topping] === true) {
+        toppingsArray.push(topping);
+      }
+    }
+
+    console.log(toppingsArray);
+
+    // switch (topping) {
+    //   case
+    // }
+  };
+
 }]);
