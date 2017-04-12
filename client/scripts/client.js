215550303755f37fb6cd55b3935a1628b7f6c7e4
Anna Springfield <anna.e.springfield@gmail.com>
2017-04-12 10:01:59 -0500

    add file structure and config

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,33 +1,23 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-// controller to grab data from form
-myApp.controller('FormController', ['$scope', 'MovieService', function($scope, MovieService) {
-  $scope.movies = MovieService.movies;
-  $scope.movie = {};
-  $scope.pushAndClear = function(movie) {
-    var movieCopy = angular.copy(movie);
-    $scope.movies.pushMovie(movieCopy);
-    for (var key in movie) {
-      movie[key] = '';
-    }
-  };
-}]);
-
-// controller to display data on DOM
-myApp.controller('DisplayController', ['$scope', 'MovieService', function($scope, MovieService) {
-  $scope.movies = MovieService.movies;
-}]);
-
-// factory to store array of movies
-myApp.factory('MovieService', [function() {
-  var movies = {
-    movieArray: [],
-    pushMovie: function(movie) {
-      this.movieArray.push(movie);
-    }
-  };
-
-  return {
-    movies: movies,
-  };
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/order', {
+      templateUrl: 'views/templates/order.html',
+      controller: 'OrderController',
+      controllerAs: 'order'
+    })
+    .when('/cart', {
+      templateUrl: 'views/templates/cart.html',
+      controller: 'CartController',
+      controllerAs: 'cart'
+    })
+    .when('/confirmed', {
+      templateUrl: 'views/templates/confirmed.html',
+      controller: 'ConfirmedController',
+      controllerAs: 'confirmed'
+    })
+    .otherwise({
+      redirectTo: 'order'
+    });
 }]);
